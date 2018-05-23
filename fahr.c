fca3f76af3889ecd170cb4370d7b3578b401aa87
Nikita Pristupchik <nikita.pristupchik@gmail.com>
2018-05-23 06:56:04 +0300

    Код fahr.c переформатирован с помощью AStyle Formatter

--- fahr.c
+++ fahr.c
@@ -8,8 +8,8 @@ int main(int argc, char const *argv[])
 {
 	int fahr;
 
-	for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP){
-		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+	for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) {
+		printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 	}
 
 	return 0;
