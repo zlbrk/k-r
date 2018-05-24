fb4c444407f9d5b2deb44693388e096acc521624
zlbrk <nikita.pristupchik@gmail.com>
2018-05-24 09:06:18 +0300

    Новая версия nc.c - 'while' -> 'for'

--- nc.c
+++ nc.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int main (int argc, char const *argv[]){
-    long nc;
+int main (int argc, char const *argv[]) {
+    double nc;
 
-    nc=0;
-    while(getchar() != EOF)
-        ++nc;
-    printf("%ld\n", nc); // %ld - спецификатор типа long
+    nc = 0;
+    for (nc = 0; getchar() != EOF; ++nc)
+        ; // эта точка с запятой находится на новой строке, чтобы не сбивать пользователя
+    printf("%.0f\n", nc); // %ld - спецификатор типа long
     return 0;
 }
