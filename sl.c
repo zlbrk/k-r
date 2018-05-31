0e114c994b136ffbba1ad1e600d740111adb1db1
Nikita Pristupchik <nikita.pristupchik@gmail.com>
2018-05-31 21:48:56 +0300

    EDIT <sl.c> - рефакторинг функции maxStringLength()

--- sl.c
+++ sl.c
@@ -4,17 +4,14 @@ double maxStringLength (void); /* damned prototype! */
 
 int main(int argc, char const *argv[])
 {
-	long MSL; /* maximum string length */
-	MSL = (long)maxStringLength();
+	long MSL = (long) maxStringLength(); /* maximum string length */
+
 	printf("MSL = %ld\n", MSL); // %ld - long specifier
 	return 0;
 }
 
 double maxStringLength () {
 	double nc;
-
-	nc = 0;
 	for (nc = 0; getchar() != EOF; ++nc);
-	printf("%.0f\n", nc);  
 	return nc;
 }
